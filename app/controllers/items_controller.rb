3201deb7d829eb07799d42237826e37ed6e30447
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-04-27 13:22:26 +0900

    コントローラへの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,20 @@
 class ItemsController < ApplicationController
   def new
+    @item = ItemForm.new
   end
 
   def create
+    @item = ItemFrom.new(item_price)
+    if @item.save
+      redirect_to items_path, notice: '商品を登録しました。'
+    else
+      render :new
+    end
+  end
+
+  private
+  
+  def item_params
+    params.require(:item).permit(:code, :item, :price)
   end
 end
