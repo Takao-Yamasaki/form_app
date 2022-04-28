cbf5f00e6cbcaaef34ac5f754335b2fc27e39a8c
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-04-28 20:38:24 +0900

    バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   with_options presence: true do
-    validates :code
+    validates :code, uniqueness: true
     validates :item
     validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
     validates :registar, inclusion: { in: [true, false] }
